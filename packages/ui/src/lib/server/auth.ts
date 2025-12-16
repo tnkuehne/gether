aba7fe233f2b371b95e738075f5e93c86ef57241
timo <22354443+tnkuehne@users.noreply.github.com>
2025-12-16 13:30:10 +0100

    add auth

---
+++ packages/ui/src/lib/server/auth.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth/minimal";
+import { env } from "$env/dynamic/private";
+import { sveltekitCookies } from "better-auth/svelte-kit";
+import { getRequestEvent } from "$app/server";
+
+const GITHUB_CLIENT_ID = env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = env.GITHUB_CLIENT_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+    throw new Error("Missing GITHUB_CLIENT_ID or GITHUB_CLIENT_SECRET");
+}
+
+export const auth = betterAuth({
+  socialProviders: {
+      github: {
+          clientId: GITHUB_CLIENT_ID,
+          clientSecret: GITHUB_CLIENT_SECRET,
+      },
+  },
+  plugins: [sveltekitCookies(getRequestEvent)],
+});
