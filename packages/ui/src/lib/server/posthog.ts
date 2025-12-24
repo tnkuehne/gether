3452e657ac1563ae4f3f97d774ab1e57448dacf7
Timo Kühne <22354443+tnkuehne@users.noreply.github.com>
2025-12-24 11:14:46 +0100

    selfhost guide (#3)

--- packages/ui/src/lib/server/posthog.ts
+++ packages/ui/src/lib/server/posthog.ts
@@ -1,9 +1,13 @@
 import { PostHog } from "posthog-node";
-import { PUBLIC_POSTHOG_HOST, PUBLIC_POSTHOG_KEY } from "$env/static/public";
+import { env } from "$env/dynamic/public";
 
-export function createPostHogClient() {
-	const posthog = new PostHog(PUBLIC_POSTHOG_KEY, {
-		host: PUBLIC_POSTHOG_HOST,
+export function createPostHogClient(): PostHog | null {
+	if (!env.PUBLIC_POSTHOG_KEY) {
+		return null;
+	}
+
+	const posthog = new PostHog(env.PUBLIC_POSTHOG_KEY, {
+		host: env.PUBLIC_POSTHOG_HOST,
 		flushAt: 1, // Send events immediately in edge environment
 		flushInterval: 0, // Don't wait for interval
 	});
--- packages/ui/src/routes/+layout.ts
+++ packages/ui/src/routes/+layout.ts
@@ -1,12 +1,12 @@
 import { browser } from "$app/environment";
-import { PUBLIC_POSTHOG_HOST, PUBLIC_POSTHOG_KEY } from "$env/static/public";
+import { env } from "$env/dynamic/public";
 import posthog from "posthog-js";
 
 export async function load() {
-	if (browser) {
-		posthog.init(PUBLIC_POSTHOG_KEY, {
+	if (browser && env.PUBLIC_POSTHOG_KEY) {
+		posthog.init(env.PUBLIC_POSTHOG_KEY, {
 			api_host: "/adler",
-			ui_host: PUBLIC_POSTHOG_HOST,
+			ui_host: env.PUBLIC_POSTHOG_HOST,
 			persistence: "memory",
 		});
 	}
